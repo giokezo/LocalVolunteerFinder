414e3c6e2f3dade6fa24cd6297ccf824ff43aa76
Kezevadze <giorgikezevadze@yahoo.com>
2025-06-22 13:28:41 +0400

    fixed a port issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ import usersRoute from './routes/users';
 import authRoute from './routes/auth';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
